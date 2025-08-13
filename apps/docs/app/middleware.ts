36d99c6a628f09cf26464a555f4fccb0b7f6bd6f
Arav menon <aravmenon.ak@gmail.com>
2025-08-13 17:47:10 +0530

    add middleware

---
+++ apps/docs/app/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const middleware = (req: NextRequest, res: NextResponse) => {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  try {
+    jwt.verify(token, process.env.NEXTAUTH_SECRET || "o9WuIIInMWoA5jSbOj6M");
+
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,3 +1,15 @@
-export default async function Home() {
-  return <>gfdsg</>;
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return <button onClick={() => signIn()}>signIn</button>;
+  if (session) return <button onClick={() => signOut()}>signOut</button>;
+
+  return (
+    <>
+      <p>Welcome {session} </p>
+    </>
+  );
 }
