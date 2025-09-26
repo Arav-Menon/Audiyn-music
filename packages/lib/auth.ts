1e1537d9484705afcfc2cd8e98a8abbafd4c4bb0
Arav menon <aravmenon.ak@gmail.com>
2025-09-26 19:57:53 +0530

    Add jwt session

--- packages/lib/auth.ts
+++ packages/lib/auth.ts
@@ -5,6 +5,7 @@ import SpotifyProvider from "next-auth/providers/spotify";
 import { db } from "@repo/db/db";
 import { authSchema } from "./validations";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -41,10 +42,18 @@ export const authOptions: NextAuthOptions = {
 
           if (!validPassword) throw new Error("Invalid Password");
 
+          const token = jwt.sign(
+            {
+              user_id: existUser.id,
+            },
+            process.env.NEXTAUTH_SECRET!
+          );
+
           return {
             id: existUser.id,
             username: existUser.username,
             email: existUser.email,
+            token,
           };
         }
 
@@ -58,45 +67,25 @@ export const authOptions: NextAuthOptions = {
           },
         });
 
+        const token = jwt.sign(
+          {
+            user_id: createUser.id,
+          },
+          process.env.NEXTAUTH_SECRET!
+        );
+
         return {
           id: createUser.id,
           username: createUser.username,
           email: createUser.email,
+          token,
         };
       },
     }),
 
     GoogleProvider({
-      clientId:
-        "74315573190-3giosieo1b9q1j9t0gupaqckntqnqi9c.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-
-  secret: process.env.NEXTAUTH_SECRET || "o9WuIIInMWoA5jSbOj6M",
-
-  session: {
-    strategy: "jwt",
-  },
-
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      if (token) {
-        session.user = {
-          ...session.user,
-          id: token.id as string,
-          email: token.email as string,
-        } as any;
-      }
-      return session;
-    },
-  },
 };
