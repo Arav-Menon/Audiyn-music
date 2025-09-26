0fde29f6f0cadb5dea3b576c50c5db851ae83b6f
Arav menon <aravmenon.ak@gmail.com>
2025-09-26 08:02:59 +0530

    set session

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,9 +3,9 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-    const { data: session } = useSession();
-    if (!session) return <button onClick={() => signIn()}>signIn</button>;
-    if (session) return <button onClick={() => signOut()}>signOut</button>;
+  const { data: session } = useSession();
+  if (!session) return <button onClick={() => signIn()}>signIn</button>;
+  if (session) return <button onClick={() => signOut()}>signOut</button>;
 
   return (
     <>
