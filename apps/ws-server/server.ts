1ef6e6e035213eff950bfb9baad82314538ea545
Arav menon <aravmenon.ak@gmail.com>
2025-10-01 21:38:31 +0530

    Change main file name of ws server

---
+++ apps/ws-server/server.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+import { Room } from "./room";
+
+const port = 8080;
+
+const wss = new WebSocketServer({ port });
+
+const room = new Room();
+
+wss.on("connection", (socket) => {
+  console.log("User connected");
+
+  room.addUser(socket);
+
+  socket.on("disconnect", () => room.removeUser(socket));
+});
+
+console.log(`Server is running `);
