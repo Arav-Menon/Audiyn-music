dbb11d82a1ecb7dd64a673b2d85c4ba9bc44da10
Arav menon <aravmenon.ak@gmail.com>
2025-10-08 01:01:45 +0530

    Add user authentication on ws server

--- apps/ws-server/server.ts
+++ apps/ws-server/server.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer } from "ws";
 import { Room } from "./room";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 const port = 8080;
 
@@ -7,10 +8,22 @@ const wss = new WebSocketServer({ port });
 
 const room = new Room();
 
-wss.on("connection", (socket) => {
-  console.log("User connected");
+wss.on("connection", (socket, req) => {
+  console.log("user is live");
 
-  room.addUser(socket);
+  const params = new URLSearchParams(req.url?.split("?")[1]);
+  const token = params.get("token");
+
+  if (!token) {
+    socket.close(4001, "unauthorized");
+    return;
+  }
+
+  const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET!) as JwtPayload;
+
+  (socket as any).userId = decoded.id;
+
+  room.joinRoom(socket);
 
   socket.on("disconnect", () => room.removeUser(socket));
 });
