bc4ac834a6c3e45d89d1af57fcd24c51adb57b6c
Arav menon <aravmenon.ak@gmail.com>
2025-10-08 01:02:09 +0530

    Add join_room handler

--- apps/ws-server/room.ts
+++ apps/ws-server/room.ts
@@ -1,4 +1,6 @@
 import WebSocket, { WebSocketServer } from "ws";
+import { JOIN_ROOM } from "./lib/messages";
+import { db } from "@repo/db/db";
 
 interface Room_Admin {
   roomId: string;
@@ -13,6 +15,60 @@ export class Room {
     this.adminId = [];
   }
 
-  addUser(socket: WebSocket) {}
+  joinRoom(socket: WebSocket) {
+    this.addHandler(socket);
+  }
   removeUser(socket: WebSocket) {}
+
+  private addHandler(socket: WebSocket) {
+    socket.on("message", async (data) => {
+      const message = JSON.parse(data.toString());
+      if (message.type == JOIN_ROOM) {
+        const { code, password } = message.payload || {};
+        if (!code && !password)
+          return socket.send(
+            JSON.stringify({
+              type: "Error",
+              message: "Missing roomcode and password",
+            })
+          );
+
+        const findRoom = await db.room.findUnique({
+          where: { code: code },
+        });
+
+        if (!findRoom)
+          return socket.send(
+            JSON.stringify({ type: "Error", message: "room not found" })
+          );
+
+        if (findRoom.isPrivate == true) {
+          if (!password || findRoom.password !== password)
+            return socket.send(
+              JSON.stringify({
+                type: "Error",
+                message: !password ? "Enter password" : "Incorrect password",
+              })
+            );
+
+          const joinRoom = await db.roomUser.create({
+            data: {
+              user: {
+                connect: {
+                  id: (socket as any).userId,
+                },
+              },
+              roomId: findRoom.id,
+            },
+          });
+
+          socket.send(
+            JSON.stringify({
+              type: "JOIN_SUCCESS",
+            })
+          );
+        }
+      }
+    });
+  }
 }
