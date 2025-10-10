2e64681950eebd999ac9d45a92420d7ea28943a4
Arav menon <aravmenon.ak@gmail.com>
2025-10-11 01:54:40 +0530

    remove respose from parameter

--- apps/docs/app/middleware.ts
+++ apps/docs/app/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export const middleware = (req: NextRequest, res: NextResponse) => {
+export const middleware = (req: NextRequest) => {
   const token = req.cookies.get("token")?.value;
 
   if (!token) {
