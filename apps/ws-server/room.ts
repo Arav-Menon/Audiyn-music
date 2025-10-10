aa8dc70c6d501ee0bc419703ff08a6182d5895c2
Arav menon <aravmenon.ak@gmail.com>
2025-10-11 00:49:58 +0530

    implement removehandler endpoint

--- apps/ws-server/room.ts
+++ apps/ws-server/room.ts
@@ -1,57 +1,89 @@
 import WebSocket from "ws";
-import { JOIN_ROOM } from "./lib/messages";
+import { JOIN_ROOM, LEAVE_ROOM } from "./lib/messages";
 import { db } from "@repo/db/db";
 
 export class Room {
   joinRoom(socket: WebSocket) {
     this.addHandler(socket);
-    // socket.send("user has been joined");
-    // console.log("User join the room");
   }
   removeUser(socket: WebSocket) {
     console.log("User leave the room");
-    socket.send(
-      JSON.stringify({
-        message: "Disconnected",
-      })
-    );
+
+    this.removeHandler(socket);
+  }
+
+  private async removeHandler(socket: WebSocket) {
+    try {
+      const findUser = await db.user.findMany({ where: { id: "userId" } });
+
+      if (!findUser)
+        return socket.send(JSON.stringify({ message: "user not exist" }));
+
+      const leaveRoom = await db.roomUser.findMany({ where: { id: "userId" } });
+
+      socket.send(
+        JSON.stringify({ message: `user has leave the room`, room: leaveRoom })
+      );
+    } catch (err) {
+      socket.close(4002, "unauthorized");
+    }
   }
 
   private addHandler(socket: WebSocket) {
     socket.on("message", async (data) => {
-      const message = JSON.parse(data.toString());
-      if (message.type == JOIN_ROOM) {
-        const { code, password } = message.payload || {};
+      try {
+        const message = JSON.parse(data.toString());
+        if (message.type == JOIN_ROOM) {
+          const { code, password } = message.payload || {};
 
-        const codes = { code, password };
-        console.log(codes);
+          const codes = { code, password };
+          console.log(codes);
 
-        if (!code)
-          return socket.send(
-            JSON.stringify({
-              type: "Error",
-              message: "Missing roomcode and password",
-            })
-          );
-
-        const findRoom = await db.room.findUnique({
-          where: { code: code },
-        });
-
-        console.log(findRoom);
-
-        if (!findRoom) return console.log("not found the room");
-        // socket.send(
-        //   JSON.stringify({ type: "Error", message: "room not found" })
-
-        if (findRoom.isPrivate == true) {
-          if (!password || findRoom.password !== password)
+          if (!code)
             return socket.send(
               JSON.stringify({
                 type: "Error",
-                message: !password ? "Enter password" : "Incorrect password",
+                message: "Missing roomcode and password",
               })
             );
+
+          const findRoom = await db.room.findUnique({
+            where: { code: code },
+          });
+
+          console.log(findRoom);
+
+          if (!findRoom) return console.log("not found the room");
+          // socket.send(
+          //   JSON.stringify({ type: "Error", message: "room not found" })
+
+          if (findRoom.isPrivate == true) {
+            if (!password || findRoom.password !== password)
+              return socket.send(
+                JSON.stringify({
+                  type: "Error",
+                  message: !password ? "Enter password" : "Incorrect password",
+                })
+              );
+
+            const roomAdded = await db.roomUser.create({
+              data: {
+                user: {
+                  connect: {
+                    id: "13233d5f-254a-4a4f-b52f-b4498bebab82",
+                  },
+                },
+                roomId: findRoom.id,
+              },
+            });
+
+            console.log(roomAdded);
+            socket.send(
+              JSON.stringify({
+                type: "JOIN_SUCCESS",
+              })
+            );
+          }
 
           const roomAdded = await db.roomUser.create({
             data: {
@@ -71,24 +103,8 @@ export class Room {
             })
           );
         }
-
-        const roomAdded = await db.roomUser.create({
-          data: {
-            user: {
-              connect: {
-                id: "13233d5f-254a-4a4f-b52f-b4498bebab82",
-              },
-            },
-            roomId: findRoom.id,
-          },
-        });
-
-        console.log(roomAdded);
-        socket.send(
-          JSON.stringify({
-            type: "JOIN_SUCCESS",
-          })
-        );
+      } catch (err) {
+        socket.close(4002);
       }
     });
   }
