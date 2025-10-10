553f4db0f6fa88200b3d5a58e88067f8c5568113
Arav menon <aravmenon.ak@gmail.com>
2025-10-11 01:54:58 +0530

    Import comming soon page

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
+import DevPage from "./devPage";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) return <button onClick={() => signIn()}>signIn</button>;
-  if (session) return <button onClick={() => signOut()}>signOut</button>;
-
   return (
     <>
-      <p>Welcome {session} </p>
-      <Image src={"/logo.png"} alt={"logo"} width={100} height={24} />
+      <DevPage />
     </>
   );
 }
