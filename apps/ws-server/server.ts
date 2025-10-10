fda8daa738d3c9f33883376bd2fcee9b6d39bc2e
Arav menon <aravmenon.ak@gmail.com>
2025-10-10 23:45:52 +0530

    remove authentication

--- apps/ws-server/server.ts
+++ apps/ws-server/server.ts
@@ -1,6 +1,5 @@
 import { WebSocketServer } from "ws";
 import { Room } from "./room";
-import jwt, { type JwtPayload } from "jsonwebtoken";
 
 const port = 8080;
 
@@ -8,24 +7,10 @@ const wss = new WebSocketServer({ port });
 
 const room = new Room();
 
-wss.on("connection", (socket, req) => {
-  console.log("user is live");
-
-  const params = new URLSearchParams(req.url?.split("?")[1]);
-  const token = params.get("token");
-
-  if (!token) {
-    socket.close(4001, "unauthorized");
-    return;
-  }
-
-  const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET!) as JwtPayload;
-
-  (socket as any).userId = decoded.id;
-
+wss.on("connection", (socket) => {
   room.joinRoom(socket);
 
-  socket.on("disconnect", () => room.removeUser(socket));
+  socket.on("close", () => room.removeUser(socket));
 });
 
 console.log(`Server is running`);
