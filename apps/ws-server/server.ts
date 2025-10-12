b97fca250a0ad6073462db0e23af2fd7d5a35271
Arav menon <aravmenon.ak@gmail.com>
2025-10-13 04:05:28 +0530

    Add user count class

--- apps/ws-server/server.ts
+++ apps/ws-server/server.ts
@@ -1,14 +1,18 @@
 import { WebSocketServer } from "ws";
-import { Room } from "./room";
+import { Room } from "./rooms/room";
+import { JoinedMembers } from "./rooms/broadcast_member";
 
 const port = 8080;
 
 const wss = new WebSocketServer({ port });
 
 const room = new Room();
+const joinedMembers = new JoinedMembers();
 
 wss.on("connection", (socket) => {
   room.joinRoom(socket);
+
+  joinedMembers.member(socket);
 
   socket.on("close", () => room.removeUser(socket));
 });
