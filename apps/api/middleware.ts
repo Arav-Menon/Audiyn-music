40b3535000077445ad9c6ae435213e7d9f5a45ff
Arav menon <aravmenon.ak@gmail.com>
2025-10-13 05:42:06 +0530

    add  authenticate middlewares

---
+++ apps/api/middleware.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const sf = process.env.AUTH_TOKEN;
+console.log(sf);
+
+export function middleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token = req.headers["authorization"];
+
+    if (!token) {
+      return res.status(401).json({ error: "No token, authorization denied" });
+    }
+
+    const decoded = jwt.verify(token, process.env.AUTH_TOKEN!) as {
+      id: string;
+      email: string;
+    };
+
+    req.id = decoded.id;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+}
