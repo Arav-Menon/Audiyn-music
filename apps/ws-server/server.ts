3abd0a32b06c07b43fe71c4bac9241aa4b62670f
Arav menon <aravmenon.ak@gmail.com>
2025-10-15 02:28:00 +0530

    setup user middlewares

--- apps/ws-server/server.ts
+++ apps/ws-server/server.ts
@@ -1,20 +1,30 @@
 import { WebSocketServer } from "ws";
 import { Room } from "./rooms/room";
 import { JoinedMembers } from "./rooms/broadcast_member";
+import jwt from "jsonwebtoken";
+import { wsAuthMiddleware } from "./middleware";
 
-const port = 8080;
+const PORT = Number(process.env.PORT);
 
-const wss = new WebSocketServer({ port });
+const wss = new WebSocketServer({ port: PORT });
 
 const room = new Room();
 const joinedMembers = new JoinedMembers();
 
 wss.on("connection", (socket) => {
-  room.joinRoom(socket);
+  try {
+    wsAuthMiddleware(() => {
+      room.joinRoom(socket);
+    });
 
-  joinedMembers.member(socket);
+    joinedMembers.member(socket);
 
-  socket.on("close", () => room.removeUser(socket));
+    socket.on("close", () => room.removeUser(socket));
+  } catch (err) {
+    console.log(err);
+    socket.send(JSON.stringify({ type: "Error", message: err }));
+    socket.close(1008, "Invalid token");
+  }
 });
 
 console.log(`Server is running`);
