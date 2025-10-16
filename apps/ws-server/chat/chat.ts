b5c66ad39976a1300b6c4914fdb50c8b65e3c256
Arav menon <aravmenon.ak@gmail.com>
2025-10-16 14:31:27 +0530

    Implement room chats

---
+++ apps/ws-server/chat/chat.ts
@@ -0,0 +1,57 @@
+import Websocket from "ws";
+import { CHAT, JOIN_ROOM } from "../lib/messages";
+import { redisClient } from "../../../packages/redis/redis";
+import { redis } from "bun";
+
+interface Rooms {
+  socket: Websocket;
+  room: string;
+}
+
+let addToRoom: Rooms[] = [];
+
+class Message {
+  handleMessage(socket: any) {
+    try {
+      socket.on("message", async (message: any) => {
+        const parsedMessage = JSON.parse(message.toString());
+
+        if (parsedMessage.type == JOIN_ROOM) {
+          console.log(`user Joined the room ${parsedMessage.payload.roomId}`);
+
+          await redisClient.HSET(
+            `room:${parsedMessage.payload.roomId}`,
+            socket,
+            JSON.stringify({
+              joinedAt: Date.now(),
+            })
+          );
+
+          addToRoom.push({
+            socket,
+            room: parsedMessage.payload.roomId,
+          });
+        }
+
+        if (parsedMessage.type == CHAT) {
+          console.log("user want to chat");
+          let currentUser = null;
+          for (let i = 0; i < addToRoom.length; i++) {
+            if (addToRoom[i]?.socket == socket) {
+              currentUser = addToRoom[i]?.room;
+            }
+          }
+          for (let i = 0; i < addToRoom.length; i++) {
+            if (addToRoom[i]?.room == currentUser) {
+              addToRoom[i]?.socket.send(parsedMessage.payload.message);
+            }
+          }
+        }
+      });
+    } catch (err) {
+      socket.close(4002, "unauthorized");
+    }
+  }
+}
+
+export const chat = new Message();
