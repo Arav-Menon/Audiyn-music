41a40f60f94762a4d5ac6e17c247decc4dd66d03
Arav menon <aravmenon.ak@gmail.com>
2025-10-17 18:20:02 +0530

    remove api and remove providers

--- apps/docs/app/api/(global)/all-rooms/route.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@repo/db/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const fetchRoom = await db.room.findMany();
-  return NextResponse.json({ fetchRoom }, { status: 302 });
-}
