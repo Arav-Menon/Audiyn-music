63e9bd55a6bd47d8218ca70a91747a1c86566f91
Arav menon <aravmenon.ak@gmail.com>
2025-10-19 09:40:30 +0530

    Implement user verification

---
+++ apps/ws-server/verification.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const verifySocketConnection = (req: any) => {
+  const token = req.headers["authorization"];
+
+  console.log(token);
+
+  if (!token) {
+    throw new Error("Missing or invalid authorization header");
+  }
+
+  const decoded = jwt.verify(token, process.env.AUTH_TOKEN!);
+
+  return decoded;
+};
+
+export default verifySocketConnection;
