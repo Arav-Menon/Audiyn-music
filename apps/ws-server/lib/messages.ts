f987d9a3665f05418eeb42d78277c8d937a5939c
Arav menon <aravmenon.ak@gmail.com>
2025-10-27 17:44:49 +0530

    Add voting commands

--- apps/ws-server/lib/messages.ts
+++ apps/ws-server/lib/messages.ts
@@ -1,4 +1,6 @@
 export const JOIN_ROOM = "JOIN_ROOM";
 export const LEAVE_ROOM = "LEAVE_ROOM";
 export const CHAT = "CHAT";
-export const SEARCH_SONG = "SEARCH_SONG"
+export const SEARCH_SONG = "SEARCH_SONG";
+export const UPVOTE = "UP_VOTE";
+export const DOWNVOTE = "DOWN_VOTE";
