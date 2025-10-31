fbf5db2ad7448e289d1e53376e387864178fc95e
Arav menon <aravmenon.ak@gmail.com>
2025-10-31 14:53:54 +0530

    Add header file

---
+++ apps/web/components/landing/src/header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <>
+      <section>
+        <div className="flex jusitfy-center" >
+          <div>
+            <h1 className="text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 font-medium ">
+              Let the Crowd <br /> Choose the Beat!
+            </h1>
+            <span className="text-2xl text-[#d3d3d3]">
+              Vote, vibe, and decide the next track — together.
+            </span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
