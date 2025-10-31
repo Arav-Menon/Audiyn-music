a12aab227562f478fe0b4cece940a9b50da3a9ab
Arav menon <aravmenon.ak@gmail.com>
2025-10-31 14:53:23 +0530

    add gradient effect

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,10 @@ import Landing from "@/components/landing/landing";
 export default function Home() {
   return (
     <>
-      <Landing />
+      <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,rgba(255,255,255,0.15)_0%,transparent_70%)]">
+        <Landing />
+      </div>
     </>
   );
 }
+  
