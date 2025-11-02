dbedbdf6ff36bc1af659fd444feef7b053beea2a
Arav menon <aravmenon.ak@gmail.com>
2025-11-03 01:46:05 +0530

    Setup landing page components

--- apps/web/components/landing/landing.tsx
+++ apps/web/components/landing/landing.tsx
@@ -1,13 +1,21 @@
+import FaqSection from "./src/faq";
 import Feature from "./src/features";
+import Footer from "./src/footer";
 import Header from "./src/header";
+import HowItWorksPage from "./src/how-it-work";
 import Navbar from "./src/navbar";
+import Thoughts from "./src/thoughts";
 
 export default function Landing() {
   return (
     <>
       <Navbar />
       <Header />
-      <Feature/>
+      <Feature />
+      <HowItWorksPage />
+      <Thoughts />
+      <FaqSection />
+      <Footer />
     </>
   );
 }
