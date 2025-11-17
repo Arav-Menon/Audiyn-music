4f3c16a488c7de8b669013f723d2801732bfae92
Arav menon <aravmenon.ak@gmail.com>
2025-11-17 05:48:56 +0530

    Add JoinTheVibe component for room joining functionality

---
+++ apps/web/components/dashboard/open-vibe.tsx
@@ -0,0 +1,22 @@
+"use client";
+import RoomModal from "@/components/dashboard/roomModel";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function JoinTheVibe() {
+  const router = useRouter();
+
+  return (
+    <>
+      <div onClick={() => router.push("/dashboard")} className="cursor-pointer">
+        <ArrowLeft
+          className="text-white relative top-8 left-8"
+          // onClick={() => router.push("/dashboard")}
+        />
+      </div>
+      <div className="fixed inset-0 bg-[radial-gradient(ellipse_at_top,rgba(255,255,255,0.15)_0%,transparent_70%)] flex items-center justify-center">
+        <RoomModal />
+      </div>
+    </>
+  );
+}
