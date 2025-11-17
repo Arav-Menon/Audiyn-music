2231b43587595faf82c0934d775547d09c97c3e7
Arav menon <aravmenon.ak@gmail.com>
2025-11-18 03:38:43 +0530

    chnage authorization method

--- apps/ws-server/verification.ts
+++ apps/ws-server/verification.ts
@@ -1,17 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const verifySocketConnection = (req: any) => {
-  const token = req.headers["authorization"];
+  try {
+    let token = req.headers["authorization"];
 
-  console.log(token);
+    console.log(token);
+    if (!token && req.cookies && req.cookies.token) {
+      token = req.cookies.token;
+    }
 
-  if (!token) {
-    throw new Error("Missing or invalid authorization header");
+    if (!token) {
+      throw new Error("Missing or invalid authorization header");
+    }
+
+    const decoded = jwt.verify(token, process.env.AUTH_TOKEN!);
+
+    return decoded;
+  } catch (err) {
+    console.log(err);
   }
-
-  const decoded = jwt.verify(token, process.env.AUTH_TOKEN!);
-
-  return decoded;
 };
 
 export = verifySocketConnection;
