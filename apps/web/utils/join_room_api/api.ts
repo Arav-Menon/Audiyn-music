0f67e6f1c4d687a3fa8b71f58b86ac7aa54c72ed
Arav menon <aravmenon.ak@gmail.com>
2025-11-17 05:49:48 +0530

    Implement room creation and retrieval API functions

---
+++ apps/web/utils/join_room_api/api.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { API_URL } from "../api_url";
+
+interface CreateRoom {
+  name: string;
+  code: string;
+  isPrivate?: string;
+  password?: string;
+}
+
+const axiosInstence = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const createRoom = async (
+  name: string,
+  code: string,
+  isPrivate: boolean,
+  password: string
+) => {
+  const response = await axiosInstence.post("/room/create-room", {
+    name,
+    code,
+    isPrivate,
+    password: password ?? null,
+  });
+  console.table(response);
+  console.log(response.data);
+  console.log(response.headers);
+
+  const res = response.data.newRoom.id;
+
+  console.log("ROOM ID:", res);
+
+  return response.data.newRoom.id;
+};
+
+export const getRoom = async (roomId: string) => {
+  const response = await axiosInstence.get(`/room/${roomId}`);
+  return response.data;
+};
