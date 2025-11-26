33b3bc03068e900491a168f8e555add3ebdb5bce
Arav menon <aravmenon.ak@gmail.com>
2025-11-26 23:54:30 +0530

    add getItems name from localhstorage

--- apps/web/utils/api_url.ts
+++ apps/web/utils/api_url.ts
@@ -1,2 +1,4 @@
 export const API_URL = "http://localhost:3001";
 export const SOCKET_URL = "ws://localhost:8080"
+export const TOKEN = localStorage.getItem("token")
+export const ROOM_ID = localStorage.getItem("roomId")
