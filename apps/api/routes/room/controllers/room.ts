99d55420c6dce8ac3a802fe573872459ea50b5f0
Arav menon <aravmenon.ak@gmail.com>
2025-11-27 01:36:55 +0530

    change room permisson

--- apps/api/routes/room/controllers/room.ts
+++ apps/api/routes/room/controllers/room.ts
@@ -12,28 +12,25 @@ getRoomRouter.get("/:roomId", middleware, async (req, res) => {
       where: {
         id: roomId,
       },
-      include: {
-        createdBy: true,
-      },
     });
 
     if (!userRoom) return res.status(404).json({ message: "room not found" });
 
     const havePermission = userRoom?.adminId == userId;
 
-    if (havePermission) {
-      res.status(201).json({
-        message: "user room",
-        room: userRoom,
-        admin: {
-          username: userRoom.createdBy.username,
-        },
-      });
-    } else {
-      res.status(401).json({
-        message: "you don't have permission to access this room",
-      });
-    }
+    // if (havePermission) {
+    //   res.status(201).json({
+    //     message: "user room",
+    //     room: userRoom,
+    //     admin: {
+    //       username: userRoom.createdBy.username,
+    //     },
+    //   });
+    // } else {
+    //   res.status(401).json({
+    //     message: "you don't have permission to access this room",
+    //   });
+    // }
   } catch (e) {
     res.status(500).json({
       message: "Internal server error",
