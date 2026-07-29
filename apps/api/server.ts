5e9777d6f3eb322cd9551a7cd24aa0fee2cc3ddc
Arav menon <aravmenon.ak@gmail.com>
2026-07-29 11:58:48 +0530

    add recentRoomsRouter and dashboardRouter to server setup

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { userRouter } from "./routes/user/auth/auth";
 import { createRoomRouter } from "./routes/room/controllers/room.create";
 import { deleteRoomRouter } from "./routes/room/controllers/room.delete";
+import { dashboardRouter } from "./routes/dashboard/controllers/dashboard";
 import { updateUserRouter } from "./routes/user/auth/update.user";
 import { userProfileRouter } from "./routes/user/auth/user";
+import { recentRoomsRouter } from "./routes/user/controllers/recentRooms";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { getRoomRouter } from "./routes/room/controllers/room";
@@ -22,14 +24,16 @@ app.use(cookieParser());
 
 // user
 app.use("/user", userRouter);
-(app.use("/user", updateUserRouter),
+ (app.use("/user", updateUserRouter),
   app.use("/user", deleteRoomRouter),
-  app.use("/user", userProfileRouter));
+  app.use("/user", userProfileRouter),
+  app.use("/user", recentRoomsRouter));
 
 // room
 app.use("/room", getRoomRouter);
 app.use("/room", createRoomRouter);
 app.use("/room", deleteRoomRouter);
+app.use("/dashboard", dashboardRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
